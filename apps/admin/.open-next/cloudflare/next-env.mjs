9c652e63ee07eaa9476f3702d2642eed5b5a5e25
drkranich <proxymovelfads@gmail.com>
2026-06-18 22:40:29 -0300

    ok

---
+++ apps/admin/.open-next/cloudflare/next-env.mjs
@@ -0,0 +1,3 @@
+export const production = {"NEXT_PUBLIC_SUPABASE_URL":"https://mbpvzhcrimdwcqkqvoqr.supabase.co","NEXT_PUBLIC_SUPABASE_ANON_KEY":"sb_publishable_zZbCUfG-1KifCBk1VJ_NTw_-RpL9A8g"};
+export const development = {"NEXT_PUBLIC_SUPABASE_URL":"https://mbpvzhcrimdwcqkqvoqr.supabase.co","NEXT_PUBLIC_SUPABASE_ANON_KEY":"sb_publishable_zZbCUfG-1KifCBk1VJ_NTw_-RpL9A8g"};
+export const test = {};
