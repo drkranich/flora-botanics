c42268be6706b027b3ea232b6652d4378dbe4c14
drkranich <proxymovelfads@gmail.com>
2026-06-25 21:45:39 -0300

    ok

--- apps/storefront/.open-next/cloudflare/init.js
+++ apps/storefront/.open-next/cloudflare/init.js
@@ -47,7 +47,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1781832921399,
+    __BUILD_TIMESTAMP_MS__: 1782434519585,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
